9db832b8742d23134a86ef472c1e8c4d438fbe5c
Gauri Mestry <gauri.mestry@t-systems.com>
2024-08-16 19:51:26 +0530

    Shimmer changes

--- src/components/common/WidgetCard.tsx
+++ src/components/common/WidgetCard.tsx
@@ -7,6 +7,7 @@ import Button from "./Button";
 import Card from "./Card";
 import DoughnutChart from "./DoughnutChart";
 import NoChart from "./NoChart";
+import Shimmer from "./Shimmer";
 import TextChart from "./TextChart";
 
 type WidgetProps = {
@@ -19,39 +20,45 @@ export default function WidgetCard({ category }: WidgetProps): ReactElement {
   const buttonClick = () => dispatch(openWidgetModal());
 
   return (
-    <div className="flex flex-col items-center sm:flex-row md:flex-row xl-flex-row sm:flex-wrap md:flex-wrap xl:flex-wrap">
-      {filteredWidgets?.map((widget, index) => (
-        <Card
-          key={widget?.id + index}
-          Component={
-            <>
-              <h5 className="font-sans text-sm font-semibold sm:text-xl md:text-xl xl:text-xl sm:font-bold md:font-bold xl:font-bold">
-                {widget?.name}
-              </h5>
-              {widget?.chartType === "doughnut" ? (
-                <DoughnutChart widget={widget} />
-              ) : widget?.chartType === "bar" ? (
-                <BarChart widget={widget} />
-              ) : widget?.chartType === "text" ? (
-                <TextChart des={widget?.des} />
-              ) : (
-                <NoChart />
-              )}
-            </>
-          }
-        />
-      ))}
-      <Card
-        Component={
-          <div className="h-full flex flex-col items-center justify-center">
-            <Button
-              buttonClick={buttonClick}
-              bg="white"
-              flexOption="flex-row"
+    <>
+      {filteredWidgets?.length ? (
+        <div className="flex flex-col items-center sm:flex-row md:flex-row xl-flex-row sm:flex-wrap md:flex-wrap xl:flex-wrap">
+          {filteredWidgets?.map((widget, index) => (
+            <Card
+              key={widget?.id + index}
+              Component={
+                <>
+                  <h5 className="font-sans text-sm font-semibold sm:text-xl md:text-xl xl:text-xl sm:font-bold md:font-bold xl:font-bold">
+                    {widget?.name}
+                  </h5>
+                  {widget?.chartType === "doughnut" ? (
+                    <DoughnutChart widget={widget} />
+                  ) : widget?.chartType === "bar" ? (
+                    <BarChart widget={widget} />
+                  ) : widget?.chartType === "text" ? (
+                    <TextChart des={widget?.des} />
+                  ) : (
+                    <NoChart />
+                  )}
+                </>
+              }
             />
-          </div>
-        }
-      />
-    </div>
+          ))}
+          <Card
+            Component={
+              <div className="h-full flex flex-col items-center justify-center">
+                <Button
+                  buttonClick={buttonClick}
+                  bg="white"
+                  flexOption="flex-row"
+                />
+              </div>
+            }
+          />
+        </div>
+      ) : (
+        <Shimmer />
+      )}
+    </>
   );
 }
